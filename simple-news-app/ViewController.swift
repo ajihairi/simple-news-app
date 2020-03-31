b9dad6fdf4ce34eebd7f04622d95a15b73804211
Hamzhya Tinnov Hairy <hamzhyatinnov@gmail.com>
2020-04-01 01:18:21 +0700

    Initial Commit

---
+++ simple-news-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-news-app
+//
+//  Created by Hamzhya Salsatinnov Hairy on 01/04/20.
+//  Copyright © 2020 Hamzhya Salsatinnov Hairy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
